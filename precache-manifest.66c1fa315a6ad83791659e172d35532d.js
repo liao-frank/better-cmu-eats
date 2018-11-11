b6a9613e2d576ca7a33a0a17c459e7316bc74eb7
Frank Liao <recs7168@gmail.com>
2018-11-11 05:28:58 -0500

    Updates

---
+++ precache-manifest.66c1fa315a6ad83791659e172d35532d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf33ec77a7ca266733e9a0e9e8641db7",
+    "url": "/better-cmu-eats/static/media/star-yellow.cf33ec77.svg"
+  },
+  {
+    "revision": "b2d981726636839f8dc8d0c9b860a7c5",
+    "url": "/better-cmu-eats/static/media/star-black.b2d98172.svg"
+  },
+  {
+    "revision": "6d7789947170156ec0654a28929197f5",
+    "url": "/better-cmu-eats/static/media/glass-gray.6d778994.svg"
+  },
+  {
+    "revision": "1b4d167d23dc57b4eb39",
+    "url": "/better-cmu-eats/static/js/runtime~main.1b4d167d.js"
+  },
+  {
+    "revision": "5c82e313ea55897351a6",
+    "url": "/better-cmu-eats/static/js/main.5c82e313.chunk.js"
+  },
+  {
+    "revision": "8506fd4a7f04c6942c5f",
+    "url": "/better-cmu-eats/static/js/1.8506fd4a.chunk.js"
+  },
+  {
+    "revision": "5c82e313ea55897351a6",
+    "url": "/better-cmu-eats/static/css/main.8c0891e3.chunk.css"
+  },
+  {
+    "revision": "5ba69846958174965d835a9a9e887c6c",
+    "url": "/better-cmu-eats/index.html"
+  }
+];
